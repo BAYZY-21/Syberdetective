cd02b1f81571020db6684674e79e037ad80c418d
BAYZY-21 <harithhassib@gmail.com>
2026-03-15 06:41:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-const CACHE_NAME = 'cyberdetective-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => {
-    if (cached) return cached;
-    return fetch(e.request).then(res => {
-      if (!res || res.status !== 200 || res.type === 'opaque') return res;
-      const clone = res.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match('./index.html'));
-  }));
-});
+const CACHE='cyberlab-pro-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>{if(r)return r;return fetch(e.request).then(res=>{if(!res||res.status!==200||res.type==='opaque')return res;const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return res;}).catch(()=>caches.match('./index.html'));}))});
